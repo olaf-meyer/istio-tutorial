ad2124a8ea7c8954a8f5a8480e00c0adaad1e5bd
Olaf Meyer <olaf-meyer@gmx.de>
2021-01-09 21:07:22 +0100

    Added scripts to deploy demo application environment

--- scripts/run_demo_application_gateway_kubernetes.sh
+++ scripts/run_demo_application_gateway_kubernetes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #set -x
 
+NAMESPACE=$1
+if [ -z "$NAMESPACE" ]
+  then
+    NAMESPACE="istio-system"
+fi
 ## Extract project as parameter in script call
 export INGRESS_HOST=$(minikube ip)
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
