6cbca4e9818a540736c27a08029f372172c545e1
Olaf Meyer <olaf-meyer@gmx.de>
2021-01-09 21:07:22 +0100

    Added scripts to deploy demo application environment

--- scripts/run_demo_application_gateway.sh
+++ scripts/run_demo_application_gateway.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #set -x
 
+NAMESPACE=$1
+if [ -z "$NAMESPACE" ]
+  then
+    NAMESPACE="demo-app"
+fi
 ## Extract project as parameter in script call
-DEMO_APP_DOMAIN=`oc get gateway -n demo-app -o json|jq -r ".items[0].spec.servers[0].hosts[0]"`
+DEMO_APP_DOMAIN=`oc get gateway -n $NAMESPACE -o json|jq -r ".items[0].spec.servers[0].hosts[0]"`
 while true
 do curl $DEMO_APP_DOMAIN/customer
 sleep .5
